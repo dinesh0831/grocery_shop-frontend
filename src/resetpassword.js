d52b703ae99acaefbce8d1bb477c25c3f81ba6a3
dinesh0831 <81325951+dinesh0831@users.noreply.github.com>
2021-12-01 13:51:41 +0530

    Update resetpassword.js

--- src/resetpassword.js
+++ src/resetpassword.js
@@ -31,7 +31,7 @@ const handleChange=({target:{name,value}})=>{
     }
     useEffect(() => {
         getData()
-    })
+    },[])
 let navigate=useNavigate ()
     const handleSubmit=async(e)=>{
        e.preventDefault()
